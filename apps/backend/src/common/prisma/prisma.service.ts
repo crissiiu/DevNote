7e3e366748cfd9a7560bc843ad28d76d56569944
crissiiu <crissiiu.dev@gmail.com>
2026-03-12 23:02:05 +0700

    feat: implement auth register and login

--- apps/backend/src/common/prisma/prisma.service.ts
+++ apps/backend/src/common/prisma/prisma.service.ts
@@ -1,7 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 import "dotenv/config";
 
 /**
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,17 @@
+import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // Cho phép ứng dụng lắng nghe các tín hiệu tắt hệ thống để đóng kết nối DB sạch sẽ
-  app.enableShutdownHooks();
+  app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
 
   await app.listen(process.env.PORT ?? 3000);
 }
